c2cc0fa3ef8fa17d579366034693a921a9705287
Rachel250180 <owner@LAPTOP-6GUJR2MF.localdomain>
2026-03-08 01:06:25 +0900

    add route controller test

--- test/controllers/routes_controller_test.rb
+++ test/controllers/routes_controller_test.rb
@@ -1,14 +1,18 @@
 require "test_helper"
 
 class RoutesControllerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    @route = routes(:one)
+  end
+
   test "should get index" do
     get routes_url
     assert_response :success
   end
 
   test "should get show" do
-    route = Route.create!(name: "テスト", description: "説明", user_id: 1)
-    get route_url(route)
+    get route_url(@route)
     assert_response :success
   end
 
@@ -17,5 +21,37 @@ class RoutesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  
+  test "should get edit" do
+    get edit_route_url(@route)
+    assert_response :success
+  end
+
+#test create
+
+  test "should create route" do
+    assert_difference("Route.count", 1) do
+      post routes_url, params: {route: {name:        "新しいルート",
+                                        description: "説明",
+                                        user_id:     @route.user_id} }
+    end
+
+    assert_redirected_to route_url(Route.last)
+  end
+
+  test ""
+
+
+
+  test "should update route" do
+    patch route_url(@route),params: {route: {name: "更新ルート"}}
+    assert_redirected_to route_url(@route)
+  end
+
+  test "should destroy route" do
+    assert_difference("Route.count", -1) do
+      delete route_url(@route)
+    end
+
+    assert_redirected_to routes_url
+  end
 end
