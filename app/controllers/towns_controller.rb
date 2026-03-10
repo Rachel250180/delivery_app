87dc99ff10fd1afa1593c5eb5e8101190cf79b88
Rachel250180 <owner@LAPTOP-6GUJR2MF.localdomain>
2026-03-11 00:51:16 +0900

    add town controller html

--- app/controllers/towns_controller.rb
+++ app/controllers/towns_controller.rb
@@ -1,2 +1,61 @@
 class TownsController < ApplicationController
+  def index
+    if params[:name].present?
+      @towns =Town.where("name LIKE ?", "%#{params[:name]}%")
+    else
+      @towns = Town.all
+    end
+  end
+
+  def show
+    @town = Town.find(params[:id])
+    @routes = @town.routes
+  end
+
+  def new
+    @town = Town.new
+  end
+
+  def create
+    @town = Town.new(town_params)
+
+    if @town.save
+      redirect_to @town, notice: "町名を追加しました！"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @town = Town.find(params[:id])
+
+    if @town.update(town_params)
+      redirect_to @town
+    else
+      render :edit, , status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @town = Town.find(params[:id])
+    @town.destroy
+    redirect_to towns_path
+  end
+
+
+
+
+
+
+
+
+
+  private
+
+  def town_params
+    params.require(:town).permit(:name)
+  end
+
+
+
 end
--- test/controllers/towns_controller_test.rb
+++ test/controllers/towns_controller_test.rb
@@ -1,7 +1,33 @@
 require "test_helper"
 
 class TownsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+
+  #index test
+
+  test "should get index and show towns in index" do
+    town = towns(:one)
+
+    get towns_url
+    assert_response :success
+    assert_select "li", text: town.name
+  end
+
+    #show test
+
+  test "should show town and display town name" do
+    town = towns(:one)
+
+    get town_url(town)
+    assert_response :success
+    assert_select "h1", /#{town.name}/
+  end
+
+  test "should display routes of town" do
+    town = towns(:one)
+    route = Route.create!(name: "テストルート", town: town)
+
+    get town_url(town)
+
+    assert_select "li", text: route.name
+  end
 end
