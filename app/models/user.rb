fc2174ec5b823ea36ad9ef9aa0bbcf05ad6867b4
Rachel250180 <owner@LAPTOP-6GUJR2MF.localdomain>
2026-03-16 17:00:56 +0900

    make user_models and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
   validates :name, presence: true,
             length: { maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -7,4 +8,7 @@ class User < ApplicationRecord
             uniqueness: true,
             length: { maximum: 255},
             format: { with: VALID_EMAIL_REGEX }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 8 }
 end
