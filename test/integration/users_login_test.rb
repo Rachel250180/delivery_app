afe9a6de96a6b6d587ff27fec6b739bb99f2b773
Rachel250180 <owner@LAPTOP-6GUJR2MF.localdomain>
2026-03-24 16:49:07 +0900

    make users test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,12 +1,38 @@
 require "test_helper"
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:michael)
+  end
+
+  test "login with valid information" do
+    get login_path
+    assert_response :success
+    post login_path, params: { session: { email: @user.email,
+                                          password: "password" } }
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_response :success
+
+    assert_equal @user.id, session[:user_id]
+  end
+
   test "login with invalid information" do
     get login_path
     assert_response :success
     post login_path, params: { session: { email: "", password: "" } }
+    assert_not is_logged_in?
     assert_response :unprocessable_entity
     assert_not flash.empty?
     get root_path
+    assert flash.empty?
+  end
+
+  test "logout" do
+    delete logout_path
+    assert_not is_logged_in?
+    assert_response :see_other
+    assert_redirected_to root_path
+    assert_nil session[:user_id]
   end
 end
