cae540a615c95a28951d6da085ebdfc8abfbb052
Rachel250180 <owner@LAPTOP-6GUJR2MF.localdomain>
2026-03-31 16:54:43 +0900

    modify test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -120,7 +120,7 @@ test "should redirect destroy when logged in as a non-admin" do
 end
 
   test "should not allow the admin attribute to be edited via the web" do
-    log_in_as(@pther_user)
+    log_in_as(@other_user)
     assert_not @other_user.admin?
     patch user_path(@other_user), params: {
                                   user:   { password:              "password",
