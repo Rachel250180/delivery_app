2242b321831a9f39d2a0f227cebbfb429ebb90e6
Rachel250180 <owner@LAPTOP-6GUJR2MF.localdomain>
2026-04-14 20:45:31 +0900

    make user_mailer

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   delete "/logout",  to: "sessions#destroy"
 
   resources :users
-
+  resources :account_activations, only: [ :edit ]
   resources :towns do
     resources :routes
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_31_150416) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_14_051827) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -42,7 +42,10 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_31_150416) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.boolean "admin"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
+    t.string "activation_digest"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.string "email"
     t.string "name"
