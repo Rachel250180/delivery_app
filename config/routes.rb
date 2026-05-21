f7b56fab202e66de00d39b4d7f97cc742ec2a4a2
Rachel250180 <owner@LAPTOP-6GUJR2MF.localdomain>
2026-05-21 14:44:43 +0900

    title modify

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "password_resets/edit"
   get "sessions/new"
   root "static_pages#home"
+  get  "/contact", to: "static_pages#contact"
   get "/signup", to: "users#new"
   get    "/login",   to: "sessions#new"
   post   "/login",   to: "sessions#create"
