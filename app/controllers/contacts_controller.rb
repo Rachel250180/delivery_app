2dd3b5f010d6fcb09ff8543c4b14e519108f7614
Rachel250180 <owner@LAPTOP-6GUJR2MF.localdomain>
2026-06-03 02:13:12 +0900

    contact validation

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,14 +1,26 @@
 class ContactsController < ApplicationController
   def new
+    @contact = Contact.new
+  end
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.valid?
+      ContactMailer.contact_email(
+        @contact.name,
+        @contact.email,
+        @contact.message
+      ).deliver_now
+
+      redirect_to root_path, notice: "送信しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
-  def create
-    ContactMailer.contact_email(
-      params[:name],
-      params[:email],
-      params[:message]
-    ).deliver_now
+  private
 
-    redirect_to new_contact_path, notice: "送信しました"
+  def contact_params
+    params.require(:contact).permit(:name, :email, :message)
   end
 end
