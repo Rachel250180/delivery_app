8dfb88f1ff7616002337605d0fbfd722d520644a
Rachel250180 <owner@LAPTOP-6GUJR2MF.localdomain>
2026-06-05 22:33:34 +0900

    contact_mailer test change

--- test/mailers/contact_mailer_test.rb
+++ test/mailers/contact_mailer_test.rb
@@ -9,19 +9,14 @@ class ContactMailerTest < ActionMailer::TestCase
     )
 
     assert_equal "お問い合わせが届きました。", mail.subject
-
-    assert_equal(
-    [ ENV["GMAIL_ADDRESS"] || "test@example.com" ],
-      mail.to
-    )
-
-    assert_equal(
-      [ ENV["GMAIL_ADDRESS"] || "test@example.com" ],
-      mail.from
-    )
-
+    assert_equal [ ENV["GMAIL_ADDRESS"] || "test@example.com" ], mail.to
+    assert_equal [ ENV["GMAIL_ADDRESS"] || "test@example.com" ], mail.from
     assert_equal [ "test@example.com" ], mail.reply_to
 
-    assert_match "michael", mail.body.encoded
+    body = mail.html_part&.body&.decoded || mail.text_part&.body&.decoded
+
+    assert_match "michael", body
+    assert_match "test@example.com", body
+    assert_match "テストメッセージ", body
   end
 end
