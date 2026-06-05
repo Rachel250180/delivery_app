544829186872894143d88ae998db6f27a95ebfa2
Rachel250180 <owner@LAPTOP-6GUJR2MF.localdomain>
2026-06-05 17:35:54 +0900

    making honeypot

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,7 +2,10 @@ class ContactsController < ApplicationController
   def new
     @contact = Contact.new
   end
+
   def create
+    return head :bad_request if params[:website].present?
+
     @contact = Contact.new(contact_params)
 
     if @contact.valid?
