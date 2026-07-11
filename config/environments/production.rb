9d6d0caec975ce6a2169d6b8174344cee238ddc9
Rachel250180 <owner@LAPTOP-6GUJR2MF.localdomain>
2026-07-11 22:17:59 +0900

    Replace Gmail SMTP with Resend

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,10 +93,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
+    address: "smtp.resend.com",
     port: 587,
-    user_name: ENV["GMAIL_ADDRESS"],
-    password: ENV["GMAIL_APP_PASSWORD"],
+    user_name: "resend",
+    password: ENV["RESEND_API_KEY"],
     authentication: :plain,
     enable_starttls_auto: true
   }
