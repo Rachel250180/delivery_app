80f37e75c5b866c5b49153cc595f88c538f704c3
Rachel250180 <owner@LAPTOP-6GUJR2MF.localdomain>
2026-07-17 00:50:05 +0900

    Refactoring user.rb

--- app/models/route.rb
+++ app/models/route.rb
@@ -5,15 +5,15 @@ class Route < ApplicationRecord
     belongs_to :town
     belongs_to :user
 
-    validates :name,        length: { minimum: 1, maximum: 35 }, 
-                            uniqueness: { scope: :town_id } ,
+    validates :name,        length: { minimum: 3, maximum: 35 },
+                            uniqueness: { scope: :town_id },
                             presence: true
     validates :description, length: { maximum: 255 }
     validate  :route_points_limit
 
   private
 
-  #採用しているAPIのGoogleマップの経由地点が、出発点含め10点までしか登録できないため
+  # 採用しているAPIのGoogleマップの経由地点が、出発点含め10点までしか登録できないため
   def route_points_limit
     if route_points.size > 10
       errors.add(:route_points, "は10個までしか登録できません")
