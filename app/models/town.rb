fc4fb5d4840fe5070c6b86fadc0129cf1db232b8
Rachel250180 <owner@LAPTOP-6GUJR2MF.localdomain>
2026-07-16 23:24:06 +0900

    Refactoring contact, route, town, rb

--- app/models/town.rb
+++ app/models/town.rb
@@ -1,13 +1,15 @@
 class Town < ApplicationRecord
-  before_validation :nomalize_kana
-  has_many :routes, dependent: :destroy
-  validates :name,  presence: true,
+  has_many          :routes, dependent: :destroy
+
+  before_validation :normalize_kana
+  validates         :name,  
+                    presence: true,
                     uniqueness: true,
                     length: { minimum: 2, maximum: 20 }
 
   private
 
-  def nomalize_kana
+  def normalize_kana
     return if kana.blank?
 
     self.kana = kana.tr("ァ-ン", "ぁ-ん")
