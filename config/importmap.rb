1e77b222b6f8a7bc32d7cb8a0440e8c89cd86d65
re-fueki <ninja250.kawasaki@ozzio.jp>
2026-07-23 20:14:46 +0900

    making password toggle

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,12 +1,9 @@
-# Pin npm packages by running ./bin/importmap
-
 pin "application"
 
-pin "@hotwired/turbo-rails",
-    to: "turbo.min.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
 
-pin "map_url",
-    to: "map_url.js"
+pin "map_url", to: "map_url.js"
 
-pin_all_from "app/javascript/map",
-              under: "map"
+pin "password_toggle"
+
+pin_all_from "app/javascript/map", under: "map"
